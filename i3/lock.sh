7063504536f1b33f6d2ffe631df1734b15f9318d
cheldux <thomasschneider10122002@gmail.com>
2025-03-23 01:41:29 +0100

    dotfile update

--- i3/lock.sh
+++ i3/lock.sh
@@ -2,10 +2,11 @@
 
 BLANK='#00000000'
 CLEAR='#ffffff22'
-DEFAULT='#ff00ffcc'
-TEXT='#14161bee'
+DEFAULT='#214291'
+ALT='#2f5ed0'
+TEXT='#080808ee'
 WRONG='#880000bb'
-VERIFYING='#bb00bbbb'
+VERIFYING='#ff2f5ed0'
 IMG=$HOME/.config/i3/lock.png
 
 killall -q i3lock
@@ -34,8 +35,8 @@ i3lock \
 --time-color=$TEXT \
 --date-color=$TEXT \
 --layout-color=$TEXT \
---keyhl-color=$WRONG \
---bshl-color=$WRONG \
+--keyhl-color=$ALT \
+--bshl-color=$ALT \
 \
 --screen 0 \
 --clock \
@@ -45,7 +46,7 @@ i3lock \
 --date-str="%Y-%m-%d" \
 --time-pos="ix:iy+125" \
 \
---greeter-text="Welcome back" \
+--greeter-text="Locked" \
 --greeter-color=$TEXT \
 --greeter-pos="ix:iy-105" \
 --keylayout 1 \
