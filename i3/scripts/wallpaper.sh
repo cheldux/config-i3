1699fe75da38af9a3f31a428d5d77ab5ff92f9d1
cheldux <thomasschneider10122002@gmail.com>
2026-09-06 18:06:51 +0200

    some changes for i3 config & new version of kitty, removed nivm config

--- i3/scripts/wallpaper.sh
+++ i3/scripts/wallpaper.sh
@@ -4,6 +4,7 @@ last=$(tail -n 1 $HOME/.fehbg | cut -d \' -f2)
 files=($(ls $path))
 while
 choice=$(($RANDOM%${#files[@]}))
+#echo ${files[$choice]}
 [[ "$last" == "$path${files[$choice]}" ]]
 do true
 done
